1f3f25cff1a9f8d0d913ae22973ed818df643462
jordles113 <jordan.t.romero@gmail.com>
2021-03-04 10:13:15 -0600

    writes up switch statement :

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
 public class App {
     public static void main(String[] args) {
-        
+        // int value = 1; 
+        // if(value == 1) {
+        //     System.out.println("value was 1");
+        // } else if (value == 2) {
+        //     System.out.println("value was 2");
+        // } else {
+        //     System.out.println("value was greater than 2");
+        // }
+
+        int switchValue = 2; 
+
+        switch(switchValue) {
+            case 1: 
+                System.out.println("value was 1");
+                break; 
+            case 2: 
+                System.out.println("value was 2");
+                break; 
+            default:
+                System.out.println("other");
+                break; 
+        }
     }
 }
