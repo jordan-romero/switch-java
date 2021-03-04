c869fbeb5e5846fcc99e1836cf60a429ab818d0d
jordles113 <jordan.t.romero@gmail.com>
2021-03-04 10:21:15 -0600

    completes challenge

--- src/App.java
+++ src/App.java
@@ -9,7 +9,7 @@ public class App {
         //     System.out.println("value was greater than 2");
         // }
 
-        int switchValue = 2; 
+        int switchValue = 4; 
 
         switch(switchValue) {
             case 1: 
@@ -18,9 +18,42 @@ public class App {
             case 2: 
                 System.out.println("value was 2");
                 break; 
+            case 3: case 4: case 5: 
+                System.out.println("greater than 2 less than 6");
+                break; 
             default:
                 System.out.println("other");
                 break; 
+        }
+
+        char character = 'A';
+        
+        switch (character) {
+            case 'A': 
+                System.out.println("A");
+                break;
+            case 'B': 
+                System.out.println("B");
+                break;
+            case 'C': 
+                System.out.println("C");
+                break;
+            case 'D': 
+                System.out.println("D");
+                break;
+            case 'E': 
+                System.out.println("E");
+                break;
+            default: 
+                System.out.println("not found");
+                break;
+        }
+
+        String month = "March"; 
+
+        switch(month) {
+            case "March": 
+                System.out.println("SPRING");
         }
     }
 }
